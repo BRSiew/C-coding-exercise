54459f4b04a771c0a5fb1d7f6150fc867aec803b
BRSiew <brssiew07@gmail.com>
2022-06-16 14:57:20 +0100

    16/06/2022
    AngApp

---
+++ Web/AngApp/Scripts/myapp.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('MyApp', []);
+app.controller('MyController', function($scope){
+    $scope.Myval = "Welcome to Angular.js";
+
+    $scope.MyFun = function ()
+    {
+        alert('Hello World:  ');
+    }
+    $scope.MyFun2 = function (val) {
+        alert('parameter passed:  ' + val);
+    }
+});
