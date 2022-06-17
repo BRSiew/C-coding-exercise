d8a280a23bfb0cb1ffc00e73d1e7826fb8a0f0eb
BRSiew <brssiew07@gmail.com>
2022-06-17 08:41:06 +0100

    17/06/2022
    Creating my first web api

---
+++ Web/MyfirstWebAPI/Controllers/Home2Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace MyfirstWebAPI.Controllers
+{
+    public class Home2Controller : ApiController
+    {
+        // GET: Home2
+        
+            private string[] S = {"apple", "Banana", "Orange" };
+            public string[] Get()
+        {
+            return S;
+        }
+
+        // ...api/home/{id}
+        public string Get(int id)
+        {
+            return S[id];
+        }
+
+        
+    }
+}
